cfc583300b956892e35b5644f24b2a889172064a
Bhavesh D <bhavesh@local>
2023-04-27 17:25:33 +0530

    nextjs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  }, 
+  output: 'export',
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.module.rules.push({
+      test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+      loader: require.resolve("url-loader")
+    }); 
+    return config;
+  }
 }
 
 module.exports = nextConfig
